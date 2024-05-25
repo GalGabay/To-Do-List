f061adc115f24b155a3d6b8dbee460b40cbd104f
galgabay <galgabay1999@gmail.com>
2024-05-25 15:02:48 +0300

    create and index

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json());
 
+app.post('/task',(req,res) => {
+  const task = req.body.task;
+})
+
 app.listen(3001, () => {
   console.log('Server is running on port 3000');
 })
--- todolist/src/Create.jsx
+++ todolist/src/Create.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import axios from 'axios'
 
 const Create = () => {
+  const [task, setTask] = React.useState()
+  const handleClick = () => {
+    axios.post('http://localhost:3001/task', {task: task})
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
   return (
     <div>
-        <input class="form_input" type="text" name="" id="" />
-        <button class="form_button" type="button">Add</button>
+        <input class="form_input" type="text" name="" id="" onChange={(e) => {setTask(e.target.value)}} />
+        <button class="form_button" type="button" onClick={handleClick}>Add</button>
     </div>
   )
 }
